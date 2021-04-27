5b56746a1b85a3a156b99844c95717e01ca7373c
Kitajima2910 <kitajima2910@gmail.com>
2021-04-27 12:19:26 +0700

    fix card

---
+++ src/java/com/fpt/controller/guest/GuestAboutController.java
@@ -0,0 +1,86 @@
+package com.fpt.controller.guest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestAboutController extends HttpServlet {
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            String view = request.getParameter("view");
+
+            if (view == null) {
+                show(request, response);
+            } else {
+                switch (view) {
+                    case "create":
+                        break;
+                    case "insert":
+                        break;
+                    case "delete":
+                        break;
+                    case "edit":
+                        break;
+                    case "update":
+                        break;
+                    case "show":
+                        show(request, response);
+                    default:
+                        show(request, response);
+                        break;
+                }
+            }
+        } catch (IOException | ServletException e) {
+            System.out.println(e.getMessage());
+        }
+    } 
+
+    private void show(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        request.getRequestDispatcher("guest/aboutus.jsp").forward(request, response);
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
